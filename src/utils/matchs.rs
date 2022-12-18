dfffe8ed3da273de5a16095b82f40f423a33bc71
laik <laik.lj@me.com>
2022-12-18 09:40:18 +0800

    add some test example in matchs

--- src/utils/matchs.rs
+++ src/utils/matchs.rs
@@ -5,18 +5,14 @@ use super::Unstructed;
 
 pub fn matchs<'a>(
     unstructeds: &'a mut Vec<Unstructed>,
-    exprs: &'a [Expr],
+    expr: Expr,
 ) -> anyhow::Result<&'a mut Vec<Unstructed>> {
     let mut remove_indexs = vec![];
     for (index, unstructed) in unstructeds.into_iter().enumerate() {
-        for expr in exprs {
-            if _match(unstructed, expr) {
-                break;
-            }
+        if !filter(unstructed, &expr) {
             remove_indexs.push(index);
         }
     }
-
     remove_indexs.sort_by_key(|n| std::usize::MAX - n);
 
     for index in remove_indexs {
@@ -26,10 +22,10 @@ pub fn matchs<'a>(
     Ok(unstructeds)
 }
 
-fn _match(unstructed: &Unstructed, expr: &Expr) -> bool {
+fn filter(unstructed: &Unstructed, expr: &Expr) -> bool {
     match expr {
-        Expr::And { lhs, rhs, .. } => return _match(unstructed, lhs) && _match(unstructed, rhs),
-        Expr::Or { lhs, rhs, .. } => return _match(unstructed, lhs) || _match(unstructed, rhs),
+        Expr::And { lhs, rhs, .. } => return filter(unstructed, lhs) && filter(unstructed, rhs),
+        Expr::Or { lhs, rhs, .. } => return filter(unstructed, lhs) || filter(unstructed, rhs),
         Expr::Eq { field, value, .. } => {
             if let Some(s) = unstructed.0.get(field) {
                 match value {
@@ -177,18 +173,20 @@ fn _match(unstructed: &Unstructed, expr: &Expr) -> bool {
                         Value::Number(s) => {
                             for item in condition_list {
                                 if let condition::Value::Number(v) = item {
-                                    if v.eq(s) {
-                                        return true;
+                                    if !v.eq(s) {
+                                        continue;
                                     }
+                                    return true;
                                 }
                             }
                         }
                         Value::String(s) => {
                             for item in condition_list {
                                 if let condition::Value::Text(v) = item {
-                                    if v.eq(s) {
-                                        return true;
+                                    if !v.eq(s) {
+                                        continue;
                                     }
+                                    return true;
                                 }
                             }
                         }
@@ -205,22 +203,22 @@ fn _match(unstructed: &Unstructed, expr: &Expr) -> bool {
                 if let Some(s) = unstructed.0.get(field) {
                     match s {
                         Value::Number(s) => {
+                            let mut hits = vec![];
                             for item in condition_list {
                                 if let condition::Value::Number(v) = item {
-                                    if !v.eq(s) {
-                                        return true;
-                                    }
+                                    hits.push(!v.eq(s));
                                 }
                             }
+                            return hits.iter().fold(true, |acc, e| (acc == *e) && (*e == true));
                         }
                         Value::String(s) => {
+                            let mut hits = vec![];
                             for item in condition_list {
                                 if let condition::Value::Text(v) = item {
-                                    if !v.eq(s) {
-                                        return true;
-                                    }
+                                    hits.push(!v.eq(s));
                                 }
                             }
+                            return hits.iter().fold(true, |acc, e| (acc == *e) && (*e == true));
                         }
                         _ => return false,
                     }
@@ -230,61 +228,35 @@ fn _match(unstructed: &Unstructed, expr: &Expr) -> bool {
 
             return false;
         }
-        Expr::IsNull { field, value, .. } => {
-            if let Some(s) = unstructed.0.get(field) {
-                match value {
-                    condition::Value::Bool(t) => {
-                        if let Value::Bool(s) = s {
-                            return s.eq(t);
-                        }
-                        return false;
-                    }
-
-                    _ => return false,
-                }
-            }
-
-            return false;
-        }
-        Expr::IsNotNull { field, value, .. } => {
-            if let Some(s) = unstructed.0.get(field) {
-                match value {
-                    condition::Value::Bool(t) => {
-                        if let Value::Bool(s) = s {
-                            return !s.eq(t);
-                        }
-                        return false;
-                    }
-
-                    _ => return false,
-                }
-            }
-
-            return false;
-        }
+        _ => false,
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use condition::parse;
-
-    use crate::utils::from_str;
-
     use super::matchs;
+    use crate::utils::from_str;
+    use condition::parse;
 
     #[test]
     fn test_eq() {
-        let mut datas = vec![
+        let datas = vec![
             from_str(r#"{"a":123,"b":312}"#).unwrap(),
             from_str(r#"{"a":123,"d":111}"#).unwrap(),
         ];
 
-        let exprs = &vec![parse("a=123").unwrap()];
-
-        match matchs(&mut datas, &exprs) {
+        match matchs(&mut datas.clone(), parse("a=123").unwrap()) {
             Ok(r) => {
                 if r.len() != 2 {
+                    panic!("Inconsistent expected results")
+                }
+            }
+            Err(e) => panic!("simulation data error: {}", e),
+        }
+
+        match matchs(&mut datas.clone(), parse("a!=123").unwrap()) {
+            Ok(r) => {
+                if r.len() != 0 {
                     panic!("Inconsistent expected results")
                 }
             }
@@ -294,31 +266,31 @@ mod tests {
 
     #[test]
     fn test_in() {
-        let mut datas = vec![
+        let datas = vec![
             from_str(r#"{"a":1,"b":2}"#).unwrap(),
             from_str(r#"{"a":3,"d":3}"#).unwrap(),
+            from_str(r#"{"a":4,"d":3}"#).unwrap(),
         ];
 
         // where a in (1,3)
-        let exprs = &vec![parse("a ~ (1,3)").unwrap()];
-
-        match matchs(&mut datas, &exprs) {
+        match matchs(&mut datas.clone(), parse("a ~ (1,3)").unwrap()) {
             Ok(r) => {
                 if r.len() != 2 {
                     panic!("Inconsistent expected results")
                 }
+                println!("in result {:?}", r)
             }
             Err(e) => panic!("simulation data error: {}", e),
         }
 
         // where a notin (1,3)
-        let exprs = &vec![parse("a ~~ (1,3)").unwrap()];
-
-        match matchs(&mut datas, &exprs) {
+        match matchs(&mut datas.clone(), parse("a ~~ (1,3)").unwrap()) {
             Ok(r) => {
-                if r.len() == 0 {
+                if r.len() != 1 {
                     panic!("Inconsistent expected results")
                 }
+                //expect result {"a":4,"d":3}
+                println!("not in result {:?}", r);
             }
             Err(e) => panic!("simulation data error: {}", e),
         }
@@ -326,16 +298,14 @@ mod tests {
 
     #[test]
     fn test_like() {
-        let mut datas = vec![
+        let datas = vec![
             from_str(r#"{"name":"bobo","src":2}"#).unwrap(),
             from_str(r#"{"name":"bill","src":3}"#).unwrap(),
             from_str(r#"{"name":"alex","src":3}"#).unwrap(),
         ];
 
         // where name like '^b'
-        let exprs = &vec![parse("name ! '^b.'").unwrap()];
-
-        match matchs(&mut datas, &exprs) {
+        match matchs(&mut datas.clone(), parse("name ! '^b.'").unwrap()) {
             Ok(r) => {
                 if r.len() != 2 {
                     panic!("Inconsistent expected results")
@@ -347,14 +317,88 @@ mod tests {
         }
 
         // where name no like '^b'
-        let exprs = &vec![parse("name !! '^b.'").unwrap()];
-
-        match matchs(&mut datas, &exprs) {
+        match matchs(&mut datas.clone(), parse("name !! '^b.'").unwrap()) {
             Ok(r) => {
-                if r.len() != 0 {
+                if r.len() != 1 {
+                    // TODO:.
                     panic!("Inconsistent expected results")
                 }
                 println!("not like data {:?}", r);
+            }
+            Err(e) => panic!("simulation data error: {}", e),
+        }
+    }
+
+    #[test]
+    fn test_compare() {
+        let datas = vec![
+            from_str(r#"{"name":"bobo","src":2}"#).unwrap(),
+            from_str(r#"{"name":"bill","src":3}"#).unwrap(),
+            from_str(r#"{"name":"alex","src":3}"#).unwrap(),
+        ];
+
+        // where src>2
+        match matchs(&mut datas.clone(), parse("src>2").unwrap()) {
+            Ok(r) => {
+                if r.len() != 2 {
+                    panic!("Inconsistent expected results")
+                }
+
+                println!("gt data {:?}", r);
+            }
+            Err(e) => panic!("simulation data error: {}", e),
+        }
+
+        // where src>=2
+        match matchs(&mut datas.clone(), parse("src>=2").unwrap()) {
+            Ok(r) => {
+                if r.len() != 3 {
+                    panic!("Inconsistent expected results")
+                }
+                println!("gte data {:?}", r);
+            }
+            Err(e) => panic!("simulation data error: {}", e),
+        }
+
+        // where src<3
+        match matchs(&mut datas.clone(), parse("src<3").unwrap()) {
+            Ok(r) => {
+                if r.len() != 1 {
+                    panic!("Inconsistent expected results")
+                }
+                println!("lt data {:?}", r);
+            }
+            Err(e) => panic!("simulation data error: {}", e),
+        }
+
+        // where src<=2
+        match matchs(&mut datas.clone(), parse("src<=2").unwrap()) {
+            Ok(r) => {
+                if r.len() != 1 {
+                    panic!("Inconsistent expected results")
+                }
+                println!("lte data {:?}", r);
+            }
+            Err(e) => panic!("simulation data error: {}", e),
+        }
+    }
+
+    #[test]
+    fn test_joint() {
+        let datas = vec![
+            from_str(r#"{"name":"bobo","src":2}"#).unwrap(),
+            from_str(r#"{"name":"bill","src":3}"#).unwrap(),
+            from_str(r#"{"name":"alex","src":3}"#).unwrap(),
+        ];
+
+        // where name like '^a' or src=3
+        match matchs(&mut datas.clone(), parse("name ! '^a.' || src=3").unwrap()) {
+            Ok(r) => {
+                if r.len() != 2 {
+                    panic!("Inconsistent expected results")
+                }
+
+                println!("joint data {:?}", r);
             }
             Err(e) => panic!("simulation data error: {}", e),
         }
