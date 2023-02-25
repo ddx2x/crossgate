2e09511be787b19afc7c05e6a213fb280cad9f27
laik <laik.lj@me.com>
2023-02-25 23:05:24 +0800

    xxx

--- src/utils/value.rs
+++ src/utils/value.rs
@@ -1,5 +1,5 @@
 use serde::de::DeserializeOwned;
-use serde_json::Value;
+use serde_json::{Map, Value};
 
 pub fn get<T>(value: Value, default: T) -> T
 where
@@ -11,10 +11,19 @@ where
     }
 }
 
+pub fn map_get<T: DeserializeOwned>(map: Map<String, Value>, key: &str, default: T) -> T {
+    if let Some(value) = map.get(key) {
+        return get(value.clone(), default);
+    }
+    default
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::utils::value::map_get;
+
     use super::get;
-    use serde_json::json;
+    use serde_json::{json, Map};
 
     #[test]
     fn test_string() {
@@ -24,5 +33,16 @@ mod tests {
     #[test]
     fn test_u64() {
         assert_eq!(123, get::<u64>(json!(123), 0));
+    }
+
+    #[test]
+    fn test_map_get_string() {
+        let mut map = Map::new();
+        map.insert("name".to_owned(), "abc".into());
+
+        assert_eq!(
+            "abc".to_string(),
+            map_get::<String>(map, "name", "".to_string())
+        );
     }
 }
