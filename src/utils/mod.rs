3d37c8220cb781b601c94bca4b097df2af47347a
laik <laik.lj@me.com>
2023-02-26 09:40:26 +0800

    update unstructed add match & get type function

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,8 @@
+use condition::parse;
 use serde::de::DeserializeOwned;
 use serde_json::{Map, Value};
+
+use self::value::map_get;
 
 pub mod dict;
 pub mod matchs;
@@ -30,8 +33,53 @@ impl Unstructed {
     pub fn keys(&self) -> Vec<String> {
         self.0.keys().map(|key| key.to_string()).collect()
     }
+
+    pub fn get_by_type<T: DeserializeOwned>(&self, key: &str, default: T) -> T {
+        map_get::<T>(&self.0, key, default)
+    }
+
+    pub fn match_by_predicate(&self, predicate: &str) -> anyhow::Result<bool> {
+        if matchs::matchs(&mut vec![self.clone()], parse(predicate)?)?.len() > 0 {
+            return Ok(true);
+        }
+        Ok(false)
+    }
 }
 
 pub fn from_str(s: &str) -> anyhow::Result<Unstructed> {
     Ok(serde_json::from_str::<Unstructed>(s)?)
+}
+
+pub fn empty_unstructed() -> Unstructed {
+    from_str("{}").unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::from_str;
+
+    #[test]
+    fn test_basic() {
+        let unstructed = match from_str(r#"{"abc":123,"name":"lijim"}"#) {
+            Ok(item) => item,
+            Err(e) => panic!("{}", e),
+        };
+
+        assert_eq!(unstructed.get_by_type::<u64>("abc", 0), 123);
+
+        assert_eq!(unstructed.match_by_predicate("abc=123").unwrap(), true);
+
+        assert_eq!(
+            unstructed.match_by_predicate("name='lijimin'").unwrap(),
+            false
+        );
+
+        assert_eq!(
+            unstructed
+                .match_by_predicate("name='lijim' && abc=123")
+                .unwrap(),
+            true
+        );
+    }
 }
